4178e15173f65a657dbb8229fcc726dd77ad7b32
nurnobimiraj00 <nurnobimiraj00@gmail.com>
2021-02-04 04:00:14 +0600

    map

--- map-filter.js
+++ map-filter.js
@@ -1,8 +1,17 @@
 const letsDoSquare = [3, 6, 4, 8, 9, 7, 5, 1, 12];
-let output = [];
-for (let i = 0; i < letsDoSquare.length; i++) {
-    const element = letsDoSquare[i];
-    let result  = element * element;
-    output.push(result);
-}
-console.log(output);
+// let output = [];
+// for (let i = 0; i < letsDoSquare.length; i++) {
+//     const element = letsDoSquare[i];
+//     let result  = element * element;
+//     output.push(result);
+// }
+
+
+// const result = letsDoSquare.map(function(element){
+//     return element * element;
+// })
+
+// const result = letsDoSquare.map(m => m*m);
+
+const result = letsDoSquare.filter(x => x/2);
+console.log(result);
